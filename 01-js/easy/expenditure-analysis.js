8f0986d3cb3f1938c23298ba98de56cc800a4656
Kunal Ladhani <kunal.ladhani@goniyo.com>
2023-12-09 15:48:29 +0530

    exp Analysis solved in o(2*n)

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,17 +4,39 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const temp = {};
+
+  for (txn of transactions) {
+    const { category, price } = txn;
+
+    if (!temp[category]) {
+      temp[category] = 0;
+    }
+
+    temp[category] += price;
+  }
+
+  const totalSpendByCategory = [];
+  for (category in temp) {
+    totalSpendByCategory.push(
+      {
+        category,
+        totalSpent: temp[category]
+      }
+    )
+  }
+
+  return totalSpendByCategory;
 }
 
 module.exports = calculateTotalSpentByCategory;
