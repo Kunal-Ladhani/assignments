6b39c981c113437f873ccecf18ce128b5516e92d
Kunal Ladhani <kunal.ladhani@goniyo.com>
2023-12-09 16:45:50 +0530

    is palindorme solved in O(n) using 2-ptr

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,36 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+const isAlpha = (char) => {
+  if ('abcdefghijklmnopqrstuvwxyz'.indexOf(char) !== -1) {
+    return true;
+  }
+  return false;
+};
+
 function isPalindrome(str) {
+  const lowerCaseStr = str.trim().toLowerCase();
+  const n = lowerCaseStr.length;
+  if (n == 0 || n == 1) {
+    return true;
+  }
+
+  let l = 0, r = n - 1;
+  while (l < r) {
+
+    if (!isAlpha(lowerCaseStr[l])) {
+      l++;
+      continue;
+    } else if (!isAlpha(lowerCaseStr[r])) {
+      r--;
+      continue;
+    } else if (lowerCaseStr[l] !== lowerCaseStr[r]) {
+      return false;
+    }
+
+    l++;
+    r--;
+  }
   return true;
 }
 
