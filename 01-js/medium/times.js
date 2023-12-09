365d7d098b3595cb5b275ebe810398958128114d
Kunal Ladhani <kunal.ladhani@goniyo.com>
2023-12-09 16:53:31 +0530

    completed times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,19 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const start = new Date().getTime();
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i;
+    }
+    return `${new Date().getTime() - start} ms`;
 }
+
+console.log(calculateTime(100));
+console.log(calculateTime(1000));
+console.log(calculateTime(10000));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000));
+console.log(calculateTime(10000000));
+console.log(calculateTime(100000000));
+console.log(calculateTime(1000000000));
